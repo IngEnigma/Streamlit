fbdf18ecb9bd9ca4802b3be698c470b4fcfaf284
IngEnigma <zs22019956@estudiantes.uv.mx>
2025-02-18 16:25:50 -0600

    finish

--- hello.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title('My first app with streamlit')
-st.header('app by IngEnigma')
-
-st.write('Ejemplo de una app con streamlit')
-st.write('Jesus Abraham Contreras Meneses')
-st.write('zs22019956')
-st.image("credencial.png")
